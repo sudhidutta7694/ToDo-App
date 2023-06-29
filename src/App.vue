e81ee6d1219d14fe752fd4a72d8e54e57d815e4a
Sudhi Sundar Dutta <sudhi.dutta7694@gmail.com>
2023-06-30 01:30:22 +0530

    Made a Header Component

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import ToDoHeader from './components/ToDoHeader.vue'
+</script>
+
+<template>
+  <ToDoHeader />
+  <RouterView />
+</template>
---
+++ src/components/ToDoHeader.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class=" from-slate-300 flex justify-between items-center py-4 px-8 sm:px-12 md:px-16 lg:20 xl:24">
+        <div class="left-container flex justify-center items-center">
+            <div class="w-24 img-container rounded-full">
+            <img class="cursor-pointer hover:scale-110 hover:drop-shadow-lg shadow-indigo-500 transition-all ease-in duration-300" src="https://thumbs.gfycat.com/HeftyDescriptiveChimneyswift-size_restricted.gif" alt="" srcset="">
+            </div>
+            <div class="name">
+                <p class="font-serif text-3xl font-bold">Todo App</p>
+            </div>
+        </div>
+        <div class="right-container flex space-x-3 text-2xl font-serif font-semibold items-center">
+            <p class="home cursor-pointer hover:text-indigo-700 transition-colors duration-300">History</p>
+            <p class="about cursor-pointer hover:text-indigo-700 transition-colors duration-300">Others</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
